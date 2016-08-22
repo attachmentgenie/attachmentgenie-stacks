679da04f85687e27bf6d733a79a6f4a30cf0aeeb
Bram Vogelaar <attachmentgenie@inuits.eu>
2016-08-22 13:03:46 +0200

    removing stacks not actively developed right now

--- spec/classes/bootstrap_spec.rb
+++ spec/classes/bootstrap_spec.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
 describe 'stacks::bootstrap' do
-  let :facts do
-  {
-    :osfamily       => 'RedHat',
-    :ipaddress      => '127.0.0.1',
-    :ipaddress_eth1 => '127.0.0.1',
-  }
-  end
   context 'with defaults for all parameters' do
     it { should contain_class('stacks::bootstrap') }
   end
