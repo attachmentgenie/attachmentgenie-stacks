54919dd6a4576fa4862c807fca19dac098901577
Bram Vogelaar <attachmentgenie@inuits.eu>
2016-08-22 23:40:37 +0200

    unifying feature switches

--- manifests/prometheus.pp
+++ manifests/prometheus.pp
@@ -1,19 +1,19 @@
 class stacks::prometheus (
-  $grafana_enabled    = false,
-  $prometheus_enabled = false,
+  $grafana    = false,
+  $prometheus = false,
 ) {
 
   validate_bool(
-    $grafana_enabled,
-    $prometheus_enabled,
+    $grafana,
+    $prometheus,
   )
 
-  if $grafana_enabled {
-    include ::profile_apache
-    include ::profile_grafana
+  if $grafana {
+    include ::profiles::apache
+    include ::profiles::grafana
   }
 
-  if $prometheus_enabled {
-    include ::profile_prometheus
+  if $prometheus {
+    include ::profiles::prometheus
   }
 }
