b4536f9d56141df0bd330e2f5ff85356cb952a59
Bram Vogelaar <attachmentgenie@inuits.eu>
2016-09-13 13:28:08 +0200

    adding option to install scala

--- manifests/runtime.pp
+++ manifests/runtime.pp
@@ -4,12 +4,19 @@
 #  class { '::stacks::runtime': }
 #
 # @param java (Boolean) Manage java on this node.
+# @param scala (Boolean) Manage scala on this node.
 class stacks::runtime (
   $java = false,
+  $scala = false,
 ) {
-  validate_bool($java)
-
+  validate_bool(
+    $java,
+    $scala,
+  )
   if $java {
     include ::profiles::java
+  }
+  if $scala {
+    include ::profiles::scala
   }
 }
