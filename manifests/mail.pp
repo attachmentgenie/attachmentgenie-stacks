01cdd5ec80ffa9f91a7f9596e5892eb2b0c99aa2
Bram Vogelaar <bram@attachmentgenie.com>
2016-11-21 16:31:25 +0100

    taking alertmanager out of prometheus, adding mailhog

---
+++ manifests/mail.pp
@@ -0,0 +1,16 @@
+# This class can be used install mail.
+#
+# @example when declaring the mail class
+#  class { '::stacks::mail': }
+#
+# @param mailhog (Boolean) Manage mailhog on this node.
+class stacks::mail (
+  $mailhog = false,
+) {
+  validate_bool(
+    $mailhog,
+  )
+  if $mailhog {
+    include ::profiles::mailhog
+  }
+}
