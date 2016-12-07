a7df0915a563aeae95f0d8f5dc7619be6dce0ed9
Bram Vogelaar <bram.vogelaar@ing.com>
2016-12-07 18:13:24 +0100

    adding missing languages

--- manifests/runtime.pp
+++ manifests/runtime.pp
@@ -4,23 +4,41 @@
 #  class { '::stacks::runtime': }
 #
 # @param java (Boolean) Manage java on this node.
+# @param golang (Boolean) Manage golang on this node.
+# @param php (Boolean) Manage php on this node.
 # @param python (Boolean) Manage python on this node.
+# @param ruby (Boolean) Manage ruby on this node.
 # @param scala (Boolean) Manage scala on this node.
 class stacks::runtime (
   $java   = false,
+  $golang = false,
+  $php    = false,
   $python = false,
+  $ruby   = false,
   $scala  = false,
 ) {
   validate_bool(
     $java,
+    $golang,
+    $php,
     $python,
+    $ruby,
     $scala,
   )
   if $java {
     include ::profiles::java
   }
+  if $golang {
+    include ::profiles::golang
+  }
+  if $php {
+    include ::profiles::php
+  }
   if $python {
     include ::profiles::python
+  }
+  if $ruby {
+    include ::profiles::ruby
   }
   if $scala {
     include ::profiles::scala
