69de1b8fb2ca584da2bbf54d7eed96519c2ef089
Bram Vogelaar <attachmentgenie@inuits.eu>
2016-12-20 10:36:33 +0100

    merging neo4j stack into database stack, adding mongodb to database stack

--- manifests/database.pp
+++ manifests/database.pp
@@ -5,22 +5,33 @@
 #
 # @param influxdb (Boolean) Manage influxdb on this node.
 # @param mysql (Boolean) Manage mysql on this node.
+# @param neo4j (Boolean) Manage neo4j on this node.
 # @param postgresql (Boolean) Manage postgresql on this node.
 class stacks::database (
   $influxdb   = false,
+  $mongodb    = false,
   $mysql      = false,
+  $neo4j      = false,
   $postgresql = false,
 ) {
   validate_bool(
     $influxdb,
+    $mongodb,
     $mysql,
+    $neo4j,
     $postgresql,
   )
   if $influxdb {
     class { '::profiles::influxdb':}
   }
+  if $mongodb {
+    class { '::profiles::mongodb':}
+  }
   if $mysql {
     class { '::profiles::mysql':}
+  }
+  if $neo4j {
+    class { '::profiles::neo4j': }
   }
   if $postgresql {
     class { '::profiles::postgresql':}
--- spec/classes/neo4j_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-describe 'stacks::neo4j' do
-  context 'with defaults for all parameters' do
-    it { should contain_class('stacks::neo4j') }
-  end
-end
