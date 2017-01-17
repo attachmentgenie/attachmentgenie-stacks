259da07d9df7677d8ced5d1ae627c029ad45a2e9
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-01-17 14:36:37 +0100

    bumping major version due to removing puppet3 support

--- manifests/database.pp
+++ manifests/database.pp
@@ -4,6 +4,7 @@
 #  class { '::stacks::metrics': }
 #
 # @param influxdb (Boolean) Manage influxdb on this node.
+# @param mongodb (Boolean) Manage mongodb on this node.
 # @param mysql (Boolean) Manage mysql on this node.
 # @param neo4j (Boolean) Manage neo4j on this node.
 # @param postgresql (Boolean) Manage postgresql on this node.
