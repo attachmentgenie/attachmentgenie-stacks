d10f40bc95e7f37c70c5f97526592b72ffd21231
Bram Vogelaar <bram@inuits.eu>
2017-01-31 17:31:07 +0100

    removing nginx from proxy stack

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -4,19 +4,13 @@
 #  class { '::stacks::proxy': }
 #
 # @param haproxy (Boolean) Manage haproxy on this node.
-# @param nginx (Boolean) Manage nginx on this node.
 class stacks::proxy (
   $haproxy = false,
-  $nginx = false,
 ){
   validate_bool(
     $haproxy,
-    $nginx,
   )
   if $haproxy {
     class { '::profiles::haproxy': }
-  }
-  if $nginx {
-    class { '::profiles::nginx': }
   }
 }
