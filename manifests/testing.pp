2634762438529193036b78def768072b3cfc1215
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-03-21 12:32:35 +0100

    moving functionality to be able to include them in a more, generic tools stack

--- manifests/testing.pp
+++ manifests/testing.pp
@@ -3,25 +3,14 @@
 # @example when declaring the testing class
 #  class { '::stacks::testing': }
 #
-# @param develop (Boolean) Manage ability to develop on this node.
 # @param jenkins (Boolean) Manage jenkins on this node.
-# @param package (Boolean) Manage abiltiy to create packages on this node.
 class stacks::testing (
-  $develop = false,
   $jenkins = false,
-  $package = false,
 ){
-  validate_bool( $develop,
+  validate_bool(
     $jenkins,
-    $package
   )
-  if $develop {
-    class { '::profiles::develop': }
-  }
   if $jenkins {
     class { '::profiles::jenkins': }
-  }
-  if $package {
-    class { '::profiles::package': }
   }
 }
