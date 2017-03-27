6d951d9cf7441bec4f9ff5325a58c25cd7079b6e
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-03-27 16:12:42 +0200

    moving profiles around, to split relay out of metrics stack

--- manifests/monitoring.pp
+++ manifests/monitoring.pp
@@ -3,19 +3,32 @@
 # @example when declaring the monitroin class
 #  class { '::stacks::monitoring': }
 #
+# @param carbon_relay (Boolean) Manage carbon_relay on this node.
 # @param collectd (Boolean) Manage collectd on this node.
 # @param logstash (Boolean) Manage logstash on this node.
+# @param statsd (Boolean) Manage statsd on this node.
 class stacks::monitoring (
+  $carbon_relay = false,
   $collectd = false,
-  $logstash = false
+  $logstash = false,
+  $statsd = false,
 ) {
-  validate_bool( $collectd,
+  validate_bool(
+    $carbon_relay,
+    $collectd,
     $logstash,
+    $statsd,
   )
+  if $carbon_relay {
+    class { '::profiles::carbon_relay': }
+  }
   if $collectd {
     class { '::profiles::collectd': }
   }
   if $logstash {
     class { '::profiles::logstash': }
+  }
+  if $statsd {
+    class { '::profiles::statsd': }
   }
 }
