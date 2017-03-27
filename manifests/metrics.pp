b6fa27ddf8fd4ba8d75d6d4fce5c1d454feb9655
Marc Lambrichs <marc.lambrichs@gmail.com>
2017-03-27 12:58:54 +0200

    Added carbon, carbon_relay and graphite_web profiles to metrics stack

--- manifests/metrics.pp
+++ manifests/metrics.pp
@@ -1,19 +1,45 @@
+# == Class: stacks::metrics
+#
 # This class can be used install metrics.
+#
+# === Examples
 #
 # @example when declaring the metrics class
 #  class { '::stacks::metrics': }
 #
+# === Parameters
+#
+# @param carbon (Boolean) Manage carbon on this node.
+# @param carbon_relay (Boolean) Manage carbon_relay on this node.
 # @param grafana (Boolean) Manage grafana on this node.
+# @param graphite_web (Boolean) Manage graphite_web on this node.
 # @param prometheus (Boolean) Manage prometheus on this node.
+#
 class stacks::metrics (
-  $grafana    = false,
-  $prometheus = false,
+  $carbon       = false,
+  $carbon_relay = false,
+  $grafana      = false,
+  $graphite_web = false,
+  $prometheus   = false,
 ) {
-  validate_bool( $grafana,
+  validate_bool(
+    $carbon,
+    $carbon_relay,
+    $grafana,
+    $graphite_web,
     $prometheus,
   )
+  if $carbon {
+    class { '::profiles::carbon': }
+  }
+  if $carbon_relay {
+    class { '::profiles::carbon_relay': }
+  }
   if $grafana {
     class { '::profiles::grafana': }
+  }
+  if $graphite_web {
+    class { '::profiles::graphite_web': }
   }
   if $prometheus {
     class { '::profiles::prometheus': }
