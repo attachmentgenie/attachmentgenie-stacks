895d5f82320c27581b9fbf64e731d2ef52df60e9
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-04-14 10:12:39 +0200

    adding developer profile

--- manifests/testing.pp
+++ manifests/testing.pp
@@ -3,13 +3,19 @@
 # @example when declaring the testing class
 #  class { '::stacks::testing': }
 #
+# @param develop (Boolean) Manage ability to develop on this node.
 # @param jenkins (Boolean) Manage jenkins on this node.
 class stacks::testing (
+  $develop = false,
   $jenkins = false,
 ){
   validate_bool(
+    $develop,
     $jenkins,
   )
+  if $develop {
+    class { '::profiles::develop': }
+  }
   if $jenkins {
     class { '::profiles::jenkins': }
   }
