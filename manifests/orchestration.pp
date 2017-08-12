1890f3572b7183459160b02b076c0339cd44a321
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-08-12 02:16:22 +0200

    adding consul to the mix

--- manifests/orchestration.pp
+++ manifests/orchestration.pp
@@ -3,17 +3,22 @@
 # @example when declaring the orchestration class
 #  class { '::stacks::orchestration': }
 #
+# @param consul (Boolean) Manage consul on this node.
 # @param mcollective (Boolean) Manage mcollective on this node.
 # @param rundeck (Boolean) Manage rundeck on this node.
 class stacks::orchestration (
-  $mcollective      = false,
-  $rundeck          = false,
+  $consul      = false,
+  $mcollective = false,
+  $rundeck     = false,
 ) {
   validate_bool(
+    $consul,
     $mcollective,
     $rundeck,
   )
-
+  if $consul {
+    class { '::profiles::consul': }
+  }
   if $mcollective {
     class { '::profiles::mcollective': }
   }
