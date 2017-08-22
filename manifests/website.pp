c15d14b897742aa8c8680b5458d73335c3f6cccf
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-08-22 23:29:13 +0200

    adding lets encrypt profile

--- manifests/website.pp
+++ manifests/website.pp
@@ -11,18 +11,21 @@
 #
 # @param apache [Boolean] Manage apache on this node.
 # @param haproxy_balancermember [Boolean] Register this node with the haproxy instance.
+# @param letsencrypt [Boolean] Manage letsencrypt certificates.
 # @param nginx [Boolean] Manage nginx on this node.
 # @param uwsgi [Boolean] Manage uwsgi on this node.
 #
 class stacks::website (
   $apache                 = false,
   $haproxy_balancermember = false,
+  $letsencrypt            = false,
   $nginx                  = false,
   $uwsgi                  = false,
 ){
   validate_bool(
     $apache,
     $haproxy_balancermember,
+    $letsencrypt,
     $nginx,
     $uwsgi,
   )
@@ -38,6 +41,10 @@ class stacks::website (
       ipaddresses       => $::ipaddress_eth1,
       ports             => '80',
     }
+  }
+
+  if $letsencrypt {
+    class { '::profiles::letsencrypt': }
   }
 
   if $nginx {
